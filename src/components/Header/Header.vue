72a681b7258eda9e3d4c9746a04fdb3a70f4bc1a
Максим Красюк <m.krasyuk@rave.dev>
2024-11-01 02:49:56 +0300

    Отступы, кнопка авторизации

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -3,19 +3,21 @@
     <div class="container">
       <div class="sections">
         <div class="section-a">
-          <span class="header__title">Куда, собрат?</span>
+          <nuxt-link to="/" class="header__title">Куда, собрат?</nuxt-link>
         </div>
         <div class="section-main max-md:hidden">
           <search-bar class="flex-1"/>
         </div>
         <div class="section-c max-lg:hidden">
           <div class="flex flex-row justify-between w-full">
-            <div class="">
-              <Icon icon="fe:star" class="header__icon"/>
-            </div>
-            <div class="flex flex-row gap-2 items-end justify-end mr-8">
+            <div></div>
+            <div v-if="useAuthStore().token" class="flex flex-row gap-2 items-end justify-end mr-8">
               <profile-link/>
             </div>
+            <nuxt-link to="/auth" v-else class="flex gap-1 items-center hover:transition-all hover:text-coral">
+              <span class="text-[14px]">Войти</span>
+              <img src="/icons/common/login-3.svg" class="cursor-pointer" alt="Выход"/>
+            </nuxt-link>
           </div>
         </div>
       </div>
@@ -26,7 +28,7 @@
 <script setup lang='ts'>
 import { useDefaultState } from './composables/useDefault'
 import SearchBar from "~/src/components/SearchBar/SearchBar.vue";
-import {Icon} from "@iconify/vue";
+import {useAuthStore} from "~/stores/auth";
 // i18
 const i18nPrefix = "components.Header"
 const nuxtApp = useNuxtApp()
